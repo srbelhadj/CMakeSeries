15844f10cbb39f49df574b617a5ddd5c8ce9db84
srbelhadj <aalam567@gmail.com>
2024-11-10 17:34:10 -0700

    linux from 12 sum to 10 sum

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -9,6 +9,6 @@ T add (T a, T b){
 
 int main (){
 	std::cout << "Hello from C++20 with CMake Linucx/Wincdows" << std::endl;
-	std::cout << "The sum is : " << add(7,5) << std::endl;
+	std::cout << "The sum is : " << add(5,5) << std::endl;
 	return 0;
 }
